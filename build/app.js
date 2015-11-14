e252e57ef81a3233c5c0edc235a3c117c47bb188
Michael D Adams <michael.d.adams@gmail.com>
2015-11-14 04:02:02 -0800

    Sorting

--- build/app.js
+++ build/app.js
@@ -22092,11 +22092,15 @@
 		value: true
 	});
 	
+	var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
+	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 	
 	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+	
+	function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 	
@@ -22110,16 +22114,78 @@
 	
 	var _item2 = _interopRequireDefault(_item);
 	
+	function stringCompare(a, b) {
+		return a.localeCompare(b, undefined, { numeric: true, sensitivity: "base" });
+	}
+	
+	try {
+		"foo".localeCompare("bar", "i");
+	} catch (e) {
+		var _stringCompare = function _stringCompare(a, b) {
+			if (a < b) {
+				return -1;
+			} else if (a > b) {
+				return 1;
+			}
+			return 0;
+		};
+	}
+	
 	var ItemList = (function (_React$Component) {
 		_inherits(ItemList, _React$Component);
 	
-		function ItemList() {
+		function ItemList(props) {
 			_classCallCheck(this, ItemList);
 	
-			_get(Object.getPrototypeOf(ItemList.prototype), 'constructor', this).apply(this, arguments);
+			_get(Object.getPrototypeOf(ItemList.prototype), 'constructor', this).call(this, props);
+	
+			this.state = {
+				sortMethod: "datetime.asc"
+			};
 		}
 	
 		_createClass(ItemList, [{
+			key: 'comparator',
+			value: function comparator(a, b) {
+				var _state$sortMethod$split = this.state.sortMethod.split(".");
+	
+				var _state$sortMethod$split2 = _slicedToArray(_state$sortMethod$split, 2);
+	
+				var method = _state$sortMethod$split2[0];
+				var directionKey = _state$sortMethod$split2[1];
+	
+				var direction = directionKey === "asc" ? 1 : -1;
+	
+				switch (method) {
+					case "title":
+						return direction * stringCompare(a.title, b.title);
+					default:
+						var aDate = Date.parse(a.datetime);
+						var bDate = Date.parse(b.datetime);
+						var datetimeDiff = undefined;
+						if (isNaN(aDate)) {
+							datetimeDiff = 1;
+						} else if (isNaN(bDate)) {
+							datetimeDiff = -1;
+						} else {
+							datetimeDiff = Date.parse(a.datetime) - Date.parse(b.datetime);
+						}
+	
+						if (!datetimeDiff) {
+							datetimeDiff = stringCompare(a.title, b.title);
+						}
+	
+						return direction * datetimeDiff;
+				}
+			}
+		}, {
+			key: 'sortList',
+			value: function sortList(method, event) {
+				event.preventDefault();
+	
+				this.setState({ sortMethod: method });
+			}
+		}, {
 			key: 'render',
 			value: function render() {
 				var _props = this.props;
@@ -22136,6 +22202,10 @@
 					);
 				}
 	
+				var sorted = [].concat(_toConsumableArray(items));
+	
+				sorted.sort(this.comparator.bind(this));
+	
 				return _react2['default'].createElement(
 					'table',
 					null,
@@ -22150,19 +22220,51 @@
 							_react2['default'].createElement(
 								'th',
 								{ scope: 'col' },
-								'Date/Time'
+								'Date/Time',
+								_react2['default'].createElement(
+									'a',
+									{ href: '#',
+										className: "datetime.asc" === this.state.sortMethod ? "active" : "",
+										onClick: this.sortList.bind(this, "datetime.asc")
+									},
+									'▽'
+								),
+								_react2['default'].createElement(
+									'a',
+									{ href: '#',
+										className: "datetime.desc" === this.state.sortMethod ? "active" : "",
+										onClick: this.sortList.bind(this, "datetime.desc")
+									},
+									'△'
+								)
 							),
 							_react2['default'].createElement(
 								'th',
 								{ scope: 'col' },
-								'Video'
+								'Video',
+								_react2['default'].createElement(
+									'a',
+									{ href: '#',
+										className: "title.asc" === this.state.sortMethod ? "active" : "",
+										onClick: this.sortList.bind(this, "title.asc")
+									},
+									'▽'
+								),
+								_react2['default'].createElement(
+									'a',
+									{ href: '#',
+										className: "title.desc" === this.state.sortMethod ? "active" : "",
+										onClick: this.sortList.bind(this, "title.desc")
+									},
+									'△'
+								)
 							)
 						)
 					),
 					_react2['default'].createElement(
 						'tbody',
 						null,
-						items.map(function (item) {
+						sorted.map(function (item) {
 							return _react2['default'].createElement(_item2['default'], {
 								key: item.url,
 								item: item,
--- components/ItemList.js
+++ components/ItemList.js
@@ -2,7 +2,65 @@ import React from 'react';
 
 import Item from './item';
 
+function stringCompare( a, b ) {
+	return a.localeCompare( b, undefined, { numeric: true, sensitivity: "base" } );
+}
+
+try {
+	"foo".localeCompare( "bar", "i" );
+} catch ( e ) {
+	function stringCompare( a, b ) {
+		if ( a < b ) {
+			return -1;
+		} else if ( a > b ) {
+			return 1;
+		}
+		return 0;
+	}
+}
+
 export default class ItemList extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			sortMethod: "datetime.asc",
+		};
+	}
+
+	comparator( a, b ) {
+		const [ method, directionKey ] = this.state.sortMethod.split( "." );
+		let direction = directionKey === "asc" ? 1 : -1;
+
+		switch ( method ) {
+		case "title" :
+			return direction * stringCompare( a.title, b.title );
+		default :
+			let aDate = Date.parse( a.datetime );
+			let bDate = Date.parse( b.datetime );
+			let datetimeDiff;
+			if ( isNaN( aDate ) ) {
+				datetimeDiff = 1;
+			} else if ( isNaN( bDate ) ) {
+				datetimeDiff = -1;
+			} else {
+				datetimeDiff = Date.parse( a.datetime ) - Date.parse( b.datetime );
+			}
+
+			if ( ! datetimeDiff ) {
+				datetimeDiff = stringCompare( a.title, b.title );
+			}
+
+			return direction * datetimeDiff;
+		}
+	}
+
+	sortList( method, event ) {
+		event.preventDefault();
+
+		this.setState( { sortMethod: method } );
+	}
+
 	render() {
 		const { items, deleteItem, markItemAsDone, markItemAsUndone } = this.props;
 
@@ -10,18 +68,40 @@ export default class ItemList extends React.Component {
 			return <p>No saved videos yet.</p>;
 		}
 
+		let sorted = [...items];
+
+		sorted.sort( this.comparator.bind( this ) );
+
 		return (
 			<table>
 				<thead>
 					<tr>
 						<th scope="col"></th>
 						<th scope="col"></th>
-						<th scope="col">Date/Time</th>
-						<th scope="col">Video</th>
+						<th scope="col">Date/Time
+							<a href="#"
+								className={"datetime.asc" === this.state.sortMethod ? "active" : "" }
+								onClick={this.sortList.bind(this, "datetime.asc")}
+							>▽</a>
+							<a href="#"
+								className={"datetime.desc" === this.state.sortMethod ? "active" : "" }
+								onClick={this.sortList.bind(this, "datetime.desc")}
+							>△</a>
+						</th>
+						<th scope="col">Video
+							<a href="#"
+								className={"title.asc" === this.state.sortMethod ? "active" : "" }
+								onClick={this.sortList.bind(this, "title.asc")}
+							>▽</a>
+							<a href="#"
+								className={"title.desc" === this.state.sortMethod ? "active" : "" }
+								onClick={this.sortList.bind(this, "title.desc")}
+							>△</a>
+						</th>
 					</tr>
 				</thead>
 				<tbody>
-				{ items.map( item =>
+				{ sorted.map( item =>
 					<Item
 						key={item.url}
 						item={item}
